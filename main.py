4bd3fd1337680865605749d81dd8d2a2e65b49c1
JOHNKARANJA01 <jonniekaras@gmail.com>
2022-06-15 20:31:19 +0300

    commit C:\Users\Pavilion\PycharmProject\WHILELOOP\.idea\WHILELOOP.iml

--- main.py
+++ main.py
@@ -1,59 +1,15 @@
-HANGMANPICS = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
 
 import random
+from turtle import clear
 
-word_list = ["ardvark","baboon","camel"]
-chosen_word = list(random.choice(word_list))
+
+from hangman_art import hang_man_arty
+print(hang_man_arty)
+from hangman_art import HANGMANPICS
+from words import words_list
+chosen_word = list(random.choice(words_list))
 chosen_word_len = len(chosen_word)
+# print(chosen_word)
 lives = 6
 display = []
 for _ in range(chosen_word_len):
@@ -64,13 +20,19 @@ print(display)
 end_of_game = False
 while not end_of_game:
     guess = input("write down a letter you think is in the word").lower()
+    clear()
+    if guess in display:
+        print(f"you've already guessed {guess},PLEASE TRY AGAIN")
     for position in range(chosen_word_len):
         if chosen_word[position] == guess:
             display[position] = chosen_word[position]
+
     if guess not in chosen_word:
+        print(f"{guess} is what you guessed, and is not in the chosen word. So you lose a life. PLEASE TRY AGAIN")
         lives-= 1
         if lives == 0:
-            print("you lose\n",HANGMANPICS[6])
+            print("You Lost\n",HANGMANPICS[6])
+            print(chosen_word)
             end_of_game = True
         elif lives == 1:
             print(HANGMANPICS[5])
@@ -85,11 +47,12 @@ while not end_of_game:
         elif lives == 6:
             print(HANGMANPICS[0])
 
-
-
     print(display)
+
 
 
     if "_" not in display:
         end_of_game = True
         print("YOU WIN")
+
+    # print(HANGMANPICS[lives])
